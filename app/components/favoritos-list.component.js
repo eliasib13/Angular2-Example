b1b882d39936d7ab36c6df1e2b2bd3b7827aad1b
Eliasib García <eliasib.lv.12@gmail.com>
2017-04-17 13:38:12 +0100

    Created FavoritoService. - Injected to FavoritoListComponent

--- app/components/favoritos-list.component.js
+++ app/components/favoritos-list.component.js
@@ -10,18 +10,34 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var favorito_service_1 = require("../services/favorito.service");
 var FavoritosListComponent = (function () {
-    function FavoritosListComponent() {
+    function FavoritosListComponent(_favoritoService) {
+        this._favoritoService = _favoritoService;
         this.title = 'Listado de marcadores';
     }
+    FavoritosListComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        console.log('FavoritosListComponent cargado.');
+        this._favoritoService.getFavoritos().subscribe(function (result) {
+            console.log(result);
+        }, function (error) {
+            _this.errorMessage = error;
+            if (_this.errorMessage != null) {
+                console.log(_this.errorMessage);
+                alert('Error en la petición');
+            }
+        });
+    };
     return FavoritosListComponent;
 }());
 FavoritosListComponent = __decorate([
     core_1.Component({
         selector: 'favoritos-list',
-        templateUrl: 'app/views/favoritos-list.html'
+        templateUrl: 'app/views/favoritos-list.html',
+        providers: [favorito_service_1.FavoritoService]
     }),
-    __metadata("design:paramtypes", [])
+    __metadata("design:paramtypes", [favorito_service_1.FavoritoService])
 ], FavoritosListComponent);
 exports.FavoritosListComponent = FavoritosListComponent;
 //# sourceMappingURL=favoritos-list.component.js.map
--- app/components/favoritos-list.component.ts
+++ app/components/favoritos-list.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FavoritoService } from '../services/favorito.service';
+import { Favorito } from '../models/favorito';
 
 @Component({
     selector: 'favoritos-list',
-    templateUrl: 'app/views/favoritos-list.html'
+    templateUrl: 'app/views/favoritos-list.html',
+    providers: [FavoritoService]
 })
 
-export class FavoritosListComponent {
+export class FavoritosListComponent implements OnInit {
     public title: string;
+    public errorMessage;
 
-    constructor(){
+    constructor(
+        private _favoritoService: FavoritoService
+    ){
         this.title = 'Listado de marcadores';
+    }
+
+    ngOnInit() {
+        console.log('FavoritosListComponent cargado.');
+        this._favoritoService.getFavoritos().subscribe(
+            result => {
+                console.log(result);
+            },
+            error => {
+                this.errorMessage = <any>error;
+
+                if (this.errorMessage != null) {
+                    console.log(this.errorMessage);
+                    alert('Error en la petición');
+                }
+            }
+        );
     }
 }
